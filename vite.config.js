8704386db07daee48a33247ea32dfa46656e47a8
thomas2570 <thomasramesh449@gmail.com>
2026-05-01 22:51:59 +0530

    fix: Add vite config to build services page

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        services: resolve(__dirname, 'services.html')
+      }
+    }
+  }
+});
